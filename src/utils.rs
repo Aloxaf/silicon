b42be97f64f7e8d516613a357a3e1d1fc14f89cb
Aloxaf <aloxafx@gmail.com>
2019-07-14 23:22:32 +0800

    Fix bug when compiling on non-linux system

--- src/utils.rs
+++ src/utils.rs
@@ -281,5 +281,5 @@ pub fn dump_image_to_clipboard(image: &DynamicImage) -> Result<(), Error> {
 
 #[cfg(not(target_os = "linux"))]
 pub fn dump_image_to_clipboard(image: &DynamicImage) -> Result<(), Error> {
-    format_err!("This feature hasn't been implemented in your system")
+    Err(format_err!("This feature hasn't been implemented in your system"))
 }
